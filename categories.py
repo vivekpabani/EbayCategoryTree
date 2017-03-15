da3fd39c2f991c42fc852c3ca3bca7dfaf612361
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-14 19:37:00 -0500

    Implement rebuild method

--- categories.py
+++ categories.py
@@ -2,6 +2,33 @@
 
 import sys
 import getopt
+from datastore import DataStore
+from create_html import create_html_page
+from ebay_fetch import fetch_categories_from_ebay, extract_category_data
+
+
+def rebuild():
+    """
+    Build the datastore/database by fetching the category data from ebay
+    """
+
+    # fetch data and extract from xml
+    category_data_xml = fetch_categories_from_ebay()
+    category_data = extract_category_data(category_data_xml)
+
+    # connect to database
+    datastore = DataStore()
+    datastore.connect()
+
+    # create a new table. drop first if exists
+    datastore.drop_table()
+    datastore.create_table()
+
+    # insert data into datastore
+    datastore.insert_all(category_data)
+
+    # close connection
+    datastore.disconnect()
 
 
 def main():
