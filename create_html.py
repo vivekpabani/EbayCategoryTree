eba67acb6dc03ed9e930d0c17b48a1287ce301bb
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-14 19:34:01 -0500

    Implement method for creating html list from category data

--- create_html.py
+++ create_html.py
@@ -19,3 +19,25 @@ def create_html_page(cid, category_list):
     f = open(str(cid) +".html", "w+", encoding='utf-8')
     f.write(html)
     f.close()
+
+
+def create_html_list(category_list):
+    """
+    Create an html list recursively using arg data
+
+    :param category_list (list): list of category data
+
+    :return (str): html script of recursive ul
+    """
+
+    html_list = "<ul>"
+
+    for item in category_list:
+        if isinstance(item, list):
+            html_list += create_html_list(item)
+        else:
+            html_list += "<li> ID: {}  Name: {}  OfferEnable: {}</li>\n".format(item[0], item[2], "True" if item[3] else "False")
+
+    html_list += "</ul>\n"
+
+    return html_list
